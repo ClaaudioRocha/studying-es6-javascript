c2f000a7cbd0178c6152921cd1d14eae42dfe3f5
Cláudio Rocha <63866022+ClaaudioRocha@users.noreply.github.com>
2023-12-05 13:46:24 -0300

    rename all the folders && include some more code

---
+++ Arrow_Functions_with_Parameters/index.js
@@ -0,0 +1,3 @@
+// para declarar uma arrow function usando variaveis como parametro, precisamos se atentar a sintaxe! segue o exemplo abaixo.
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat(["café", "brownie"], ["capuccino", "chessecake"]));
---
+++ Use_Arrow_Functions_to_Write_Concise_Anonymous_Functions/index.js
@@ -0,0 +1,2 @@
+// as vezes nao precisamos nomear nossas funções, e eh ai onde nos usamos as funçoes anonimas usando arrow functions
+const magic = () => new Date();
